76c7b31963ee12e63ada79110eb0eb7bf0aeaf44
ptlong-qn2k <thanhlong05.06.2k@gmail.com>
2024-10-24 21:57:48 +0700

    react1 done

--- src/ComponentPage/Reports.jsx
+++ src/ComponentPage/Reports.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-import '../Styles/App.css'
-import Layout from "./Layout";
+import React from 'react';
+import '../Styles/App.css';
+import Layout from './Layout';
 const Reports = () => {
-
     return (
         <Layout>
             <div className="home">
                 <h1 className="text-center">Myreport</h1>
             </div>
         </Layout>
-
-    )
-}
-export default Reports
+    );
+};
+export default Reports;
--- src/ComponentPage/Toasttify.jsx
+++ src/ComponentPage/Toasttify.jsx
@@ -4,8 +4,6 @@ import { ToastContainer, toast } from 'react-toastify';
 const Toastify = (props) => {
     // toast("This is a toast notification !");
     toast(`${props}`);
-    return (
-        <ToastContainer />
-    )
-}
-export default Toastify
+    return <ToastContainer />;
+};
+export default Toastify;
